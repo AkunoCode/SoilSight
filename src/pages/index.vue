590b20418d61fe2fbc01d0cb0d649dffd3c23fbd
John Leo D. Echevaria <echevariajohnleo@gmail.com>
2025-10-02 16:15:22 +0800

    leaflet map with tayabas marker

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,7 +1,57 @@
 <template>
-  <HelloWorld />
+  <v-app>
+    <v-main>
+      <div id="map"></div>
+    </v-main>
+  </v-app>
 </template>
 
 <script setup>
-  //
+import { onMounted } from "vue";
+import L from "leaflet";
+import "leaflet/dist/leaflet.css";
+import { VCardTitle } from "vuetify/components";
+
+onMounted(async () => {
+  // Tayabas City coordinates (center)
+  const tayabas = [13.9649, 121.5923];
+
+  const map = L.map("map").setView(tayabas, 13);
+
+  // Base map
+  L.tileLayer("https://cartodb-basemaps-a.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png", {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/">OSM</a> contributors'
+  }).addTo(map);
+
+  // Load GeoJSON boundary (file is in src/assets/geojson/Tayabas.geojson)
+  const res = await fetch("/src/assets/geojson/Tayabas.geojson");
+  const data = await res.json();
+
+  const geoLayer = L.geoJSON(data, {
+    style: {
+      color: "#2264A2",
+      weight: 3,
+      dashArray: "5, 5",
+      fillColor: "#2264A2",
+      fillOpacity: 0.1
+    },
+    onEachFeature: (feature, layer) => {
+      if (feature.properties?.name) {
+        layer.bindPopup(feature.properties.name);
+      }
+    }
+  }).addTo(map);
+
+  // Fit map to boundary
+  map.fitBounds(geoLayer.getBounds());
+});
 </script>
+
+<style>
+#map {
+  height: 100vh;
+  width: 100vw;
+  position: absolute;
+  z-index: 0;
+}
+</style>
