74623ec3fb7ec993f1b68c3d52296b192e33d78d
John Leo D. Echevaria <echevariajohnleo@gmail.com>
2025-11-11 22:56:49 +0800

    Add environment configuration and Directus SDK integration

---
+++ src/composables/useDirectus.js
@@ -0,0 +1,10 @@
+import { createDirectus, staticToken, rest } from '@directus/sdk';
+
+const apiUrl = import.meta.env.VITE_DIRECTUS_API_URL;
+const token = import.meta.env.VITE_DIRECTUS_BEARER_TOKEN;
+
+const directus = createDirectus(apiUrl)
+    .with(staticToken(token))
+    .with(rest());
+
+export default directus;
