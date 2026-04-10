1029ce3f1e63581ddd87958b04421da52acd70a6
AkunoCode <echevariajohnleo@gmail.com>
2026-04-11 03:34:23 +0800

    chore: remove generic LoadingOverlay spinner (redundant with skeleton loaders and error messages)

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -3,11 +3,8 @@
     <v-main>
       <router-view />
     </v-main>
-    <!-- place LoadingOverlay as a sibling to v-main so its fixed positioning covers the full document -->
-    <LoadingOverlay />
   </div>
 </template>
 
 <script setup>
-import LoadingOverlay from '@/components/LoadingOverlay.vue'
 </script>
